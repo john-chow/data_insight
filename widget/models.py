f24c96e96b8d0918d61ce9c2880150cd8bf5de09
zhouzhengran <zhouzhengran@vtroncrop.com>
2014-05-26 00:14:32 -0700

    修改了reqDrawData接口里面变量的命名；增加了获得Json格式组建图像数据的接口

--- widget/models.py
+++ widget/models.py
@@ -17,8 +17,18 @@ class WidgetModel(ElementModel):
     m_pic           = models.TextField()
     m_external_db   = models.ForeignKey('ExternalDbModel')
 
+    def getExtentDict(self):
+        return { 
+            u'x': eval(self.m_x) if self.m_x else self.m_x \
+            , u'y': eval(self.m_y) if self.m_y else self.m_y \
+            , u'color': self.m_color \
+            , u'size':  self.m_size \
+            , u'graph': self.m_graph \
+        }
+
     class Meta:
         db_table = 'widgets'
+
 
 class ExternalDbModel(models.Model):
     """
