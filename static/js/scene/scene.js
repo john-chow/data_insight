00dc1905c6e7fc62b8be7ac670242ee48dc3fe36
zhouzhengran <zhouzhengran@vtroncrop.com>
2014-05-27 04:27:50 -0700

    模块化新增场景页

--- static/js/scene/scene.js
+++ static/js/scene/scene.js
@@ -22,7 +22,7 @@ window.onload = function (){
 }
 
 //动态变化高度
-function changeAuto(){
+function changeAuto() {
     var clientHeight = document.documentElement.clientHeight;
     var eHeight = $("#header").height()+$("#menu").height()+1;
     $("#scene_design_right").height(clientHeight-eHeight);
@@ -40,6 +40,7 @@ function changeAuto(){
 }
 
 
+/*
 // 点击已被允许使用的组件时，请求拿到组件chart图的数据
 $(".scene_list_widget").on("click", function(ev) {
     var wiId = $(this).attr("data-id");
@@ -68,6 +69,7 @@ var onGetWidgetData = function(data) {
         }
     })
 }
+*/
 
 
 
@@ -92,40 +94,71 @@ function outtest() {
 
 
 
+
+// 传递全局事件对象
+$body = $("body");
+
+
 // 组织区域模块
 define("compontnents", [], function() {
     // 所有可以用组建类构造函数
     var allWidgetsObj = {
+        $el:                null,
+        widgetsList:        [],
+        init:               function($el) {
+            this.$el = $el;
+
+            // 从dom树上抓取该区域内全部element，并且组合成widget对象
+            // 保存widget对象为属性成员
+            var self = this;
+            $.each(this.$el.find(".scene_list_widget"), function(i, ele) {
+                var widgetId        = $(ele).attr("data-id");
+                var widgetName      = $(ele).attr("title");
+                var widgetObj = new CWidget(widgetId, widgetName);
+
+                // 监听点击事件，以抓取图像数据的函数作回掉函数
+                $(ele).on("click", {"widget": widgetObj}
+                                , bindContext(scnWidgetsObj.respToSelect, scnWidgetsObj));
+
+                self.addWidget(widgetObj)
+            })
+        },
+
+        addWidget:          function(widgetObj) {
+            this.widgetsList.push(widgetObj)
+        }
     };
 
     // 本场景属性设计类构造函数
     var myAttributesObj = {
+        init:       function() {
+        }
     };
 
     // 本场景组件类构造函数
-    var myWidgetsObj = {
+    var scnWidgetsObj = {
         // 组件列表
-        widgets:        [],
-
-        // 组件布局
-        layout:         "", 
+        widgetsList:        [],
 
         // 对应dom节点
-        $el:            "",
+        $el:                "",
+
+        // 组件列表中组件模板
+        template:           '<li class="scene_list_widget" id="wi_{widget_id}" ' 
+                        + 'data-id={widget_id} title="{widget_name}">'
+                        + '<i class="glyphicon glyphicon-stats"></i>'
+                        + '&nbsp;{widget_name}</li>',
 
         init:           function($el) {
             this.$el = $el;
             
             // 从dom树上抓取该区域内全部element，并且组合成widget对象
-            // 保存widget对象为属性成员
+            // 模拟为被选中，加入本场景组件列表
             var self = this;
-            $.each(this.$el.find(".widget"), function(i, ele) {
-                var widgetObj = new OneWidget($(obj).attr("data-id"));
-
-                // 监听拖动事件，以抓取图像数据的函数作回掉函数
-                //
-
-                self.allWidgetsObj.push(widgetObj)
+            $.each(this.$el.find(".scene_list_widget"), function(i, ele) {
+                var widgetObj = new CWidget($(obj).attr("data-id"));
+                var ev = {"data": widgetObj};
+                self.respToSelect(ev)
             })
         },
 
@@ -133,7 +166,19 @@ define("compontnents", [], function() {
         initElements:   function() {
         },
 
-        addWidget:      function(ev) {
+        respToSelect:   function(ev) {
+            var widgetObj = ev.data["widget"];
+            this.addWidget(widgetObj);
+
+            // 向服务器请求图像数据
+            widgetObj.fetchPicData()
+        },
+
+        addWidget:      function(widgetObj) {
+            this.$el.find("#scene_widgets")
+                    .append(this.template.replace(/{widget_id}/g, widgetObj.id)
+                                        .replace(/{widget_name}/g, widgetObj.name));
+            this.widgetsList.push(widgetObj)
         },
 
         rmWidget:       function() {
@@ -144,47 +189,83 @@ define("compontnents", [], function() {
         },
 
         restore:        function() {
-        },
-
-        // 设置布局
-        setLayout:      function(layout) {
-            this.layout = layout
         }
     };
 
 
     // 每个组件类构造函数
-    var OneWidget =     function(id, name) {
+    var CWidget =     function(id, name) {
         // 组件id
         this.id     =           id;
         this.name   =           name;
-   
+
+        // 组件布局位置
+        this.layout =           "", 
+    
         // 获取组件图像数据
         this.fetchPicData =     function() {
             $.ajax({
-                url:        "/widget/" + wiId + "/"
-                , type:     "GET"
-                , success:  this.onGetWidgetData
-                , error:    function() {}
+                url:            "/widget/show/" + this.id + "/"
+                , type:         "GET"
+                , success:      this.onGetWidgetData
+                , error:        function() {}
             })
         };
 
-        this.onGetWidgetData =  function() {
-
+        this.onGetWidgetData =  function(data) {
+            // 如果成功，则传递数据到面板进行画图
+            if (data.succ){
+                $body.trigger("show_widget", data)
+            } else {
+                alert(data.msg)
+            }
         }
     };
+    
+    // 开始运行
+    allWidgetsObj.init( $("#all_widgets") );
+    scnWidgetsObj.init( $("#scene_widgets") );
+    myAttributesObj.init();
 })
 
 
 
 
 // 呈现区域模块
-define("layout", [], function() {
+define("display", ["drawer"], function(DrawManager) {
+    var display = {
+        $el:                null,
+
+        run:                function() {
+            this.initStyle();
+            this.startListener()
+        },
+
+        initStyle:          function() {
+        },
+        
+        startListener:      function() {
+            $body.on("show_widget",     bindContext(this.showWidget, this));
+            $body.on("rm_widget",       this.rmWidget);
+        },
+
+        showWidget:         function(ev, data) {
+            var gridster = $(".gridster ul").gridster().data('gridster');
+            gridster.add_widget("<li class='se_wi_"+data.data_id+"' data-id='"+data.data_id+"'></li>", 1, 1);
+            var drawer = new DrawManager();
+            var len = $(".se_wi_"+data.data_id).length-1;
+            drawer.run($(".se_wi_"+data.data_id)[len], data.data)
+        },
+
+        rmWidget:           function() {
+        }
+    };
+
+    // 启动
+    display.run()
 })
 
 
-define('', ["compontnents", "layout"], function() {
-    require("compontnents");
-    require("layout");
+require(["compontnents", "display"], function() {
 })
 
--- templates/widget/add/js/common/tools.js
+++ templates/widget/add/js/common/tools.js
@@ -139,6 +139,26 @@ function cloneObject(obj) {
 }
 
 
+// 绑定函数执行的上下文
+// 抄underscore
+function bindContext(func, context) {
+    var nativeBind = Function.prototype.bind;
+    var slice = Array.prototype.slice;
+    var args, bound;
+    if (nativeBind && func.bind === nativeBind) 
+        return nativeBind.apply(func, slice.call(arguments, 1));
+    if (!_.isFunction(func)) throw new TypeError;
+    args = slice.call(arguments, 2);
+    return bound = function() {
+        if (!(this instanceof bound)) return func.apply(context, args.concat(slice.call(arguments)));
+        ctor.prototype = func.prototype;
+        var self = new ctor;
+        ctor.prototype = null;
+        var result = func.apply(self, args.concat(slice.call(arguments)));
+        if (Object(result) === result) return result;
+        return self;
+    }
+}
 
 
 
