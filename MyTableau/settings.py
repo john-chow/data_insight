a36de3a9fc24bcf4eb50eb67debcf4f9e6066cf4
zhouzhengran <zhouzhengran@vtroncrop.com>
2014-09-03 15:50:31 +0800

    settings

--- MyTableau/settings.py
+++ MyTableau/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': '123456',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '10.1.50.125',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
