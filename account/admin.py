ba89725dbe9e6cd0e4ebd545591b32940676017f
zhouzhengran <zhouzhengran@vtroncrop.com>
2014-09-10 19:48:15 +0800

    solve accont model bug

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from account.models import AccountModel
+#from account.models import AccountModel
 
-admin.site.register(AccountModel)
+#admin.site.register(AccountModel)
