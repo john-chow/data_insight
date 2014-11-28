cc586c1da22685b0490f139e820f8d6c219589cb
zhouzhengran <zhouzhengran@vtroncrop.com>
2014-11-28 10:35:49 +0800

    echarts模块化引入改为单文件引入

--- widget/forms.py
+++ widget/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 
 class ConnDbForm(forms.Form):
-	ip			= forms.GenericIPAddressField(initial='10.1.50.125')
+	ip			= forms.GenericIPAddressField(initial='10.1.50.96')
 	port		= forms.IntegerField(max_value=65535, min_value=0, required=False, initial='5432')
 	db			= forms.CharField(initial='data_insight')
 	user		= forms.CharField(initial='postgres')
